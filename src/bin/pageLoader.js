befdeb4567efbebae525e1a97f82107059e31baf
PeresvetS <niks280593@gmail.com>
2017-04-21 10:28:02 +0300

    added debug and console.error

--- src/bin/pageLoader.js
+++ src/bin/pageLoader.js
@@ -4,13 +4,13 @@ import program from 'commander';
 import pageLoader from '../';
 
 program
-  .version('0.3.2')
+  .version('0.3.6')
   .description('Download the website and use it locally.')
   .option('-o, --output [path]', 'Path to save the website locally')
   .arguments('<url>')
   .action((url) => {
     pageLoader(url, program.output)
-      .catch(err => console.log(err))
-      .then(() => { console.log(`The website ${url} is successfully saved locally`); });
+      .then(msg => console.log(msg))
+      .catch(err => console.error(err));
   })
   .parse(process.argv);
